933dc8057b88e28452cd6f0f9244c7aefb794b8a
Mayank Binayake <mayankbinayake@gmail.com>
2020-03-08 16:18:09 +0530

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Model.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Values> Values { get; set; }
+    }
+}
---
+++ DatingApp.API/Model/Values.cs
@@ -0,0 +1,9 @@
+namespace DatingApp.API.Model
+{
+    public class Values
+    {
+        public int ID { get; set; }
+
+        public string Value { get; set; }
+    }
+}
